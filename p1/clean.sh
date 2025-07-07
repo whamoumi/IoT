85a61abdfeee2e7e30134ff7133ca9989f8d8b3c
Votre Nom <Vous@exemple.com>
2025-07-07 12:06:27 +0200

    ajout de iot

---
+++ p1/clean.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "VMs actuellement gérées par libvirt:"
+virsh list --all
+for domain in $(virsh list --name); do
+    echo "Arrêt de la VM: $domain"
+    virsh destroy "$domain"
+done
+
+for domain in $(virsh list --all --name); do
+    echo "Suppression de la VM: $domain"
+    virsh undefine "$domain" --remove-all-storage
+done
+
+rm -rf ./.vagrant
+vagrant destroy -f
+echo "Toutes les VMs ont été supprimées."
