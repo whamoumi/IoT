47965588385867d544784c4a512cc9e65bd96273
Your Name <you@example.com>
2025-07-11 11:28:15 +0200

    ajout de p3

---
+++ p3/launch.sh
@@ -0,0 +1,63 @@
+# 1. Créer le cluster k3d avec les bons ports exposés
+k3d cluster create ArgocdDeployment \
+  --port "8080:443@loadbalancer" \
+  --port "8888:8888@loadbalancer" \
+
+# 2. Créer les namespaces nécessaires
+kubectl create namespace dev
+kubectl create namespace argocd
+
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.9.6/deploy/static/provider/kind/deploy.yaml
+
+# 3. Installer Argo CD
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+
+# 4. Attendre que tous les pods Argo CD soient prêts
+kubectl wait --for=condition=Ready pod --all -n argocd --timeout=120s
+kubectl wait --for=condition=Ready pod -l app.kubernetes.io/name=argocd-server -n argocd --timeout=180s
+
+# 5. Déployer ton application
+
+kubectl apply -n dev -f ./config/deployment.yaml
+
+# 6. Attendre que les pods et déploiements dans `dev` soient prêts
+kubectl wait --for=condition=Ready pod --all -n dev --timeout=120s
+
+kubectl wait --for=condition=Ready deployment --all -n dev --timeout=120s
+
+
+# 7. Exposer les services en LoadBalancer (pour accès via localhost)
+nohup kubectl port-forward svc/argocd-server -n argocd 8080:443 >/dev/null 2>&1 &
+nohup kubectl port-forward svc/app-one -n dev 8888:8888 >/dev/null 2>&1 &
+
+
+
+
+
+sleep 5
+# 8. Attendre que les services soient exposés
+echo "⏳ Recherche du secret argocd-initial-admin-secret..."
+# Récupération du mot de passe admin
+PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
+echo "🔑 Mot de passe admin Argo CD: $PASSWORD"
+sleep 5
+#Login à Argo CD
+argocd login localhost:8080 --username admin --password $PASSWORD --insecure
+# 9. Login à Argo CD
+
+# 10. Créer l'application Argo CD
+argocd app create development \
+  --repo https://github.com/whamoumi/dev \
+  --path manifests \
+  --dest-server https://kubernetes.default.svc \
+  --dest-namespace dev \
+  --sync-policy automated
+
+# 11. Synchroniser l'application manuellement si nécessaire
+# argocd app sync development
+
+# 12. Afficher l'URL d'accès
+echo "🔗 Argo CD: https://localhost:8080"
+echo "🚀 App one: http://localhost:8888"
